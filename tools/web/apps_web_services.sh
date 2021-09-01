fcf5789499301b1f6265b967c801314ebad1b4c7
Fabio Kruger <10956489+krufab@users.noreply.github.com>
2021-09-01 21:46:59 +0200

    Cleaned code

--- tools/web/apps_web_services.sh
+++ tools/web/apps_web_services.sh
@@ -44,7 +44,6 @@ function configure_web_services {
     DB_HOST="$(get_MP_D_CONTAINER_x "${config_file}" "mariadb" "mariadb")"
     POSTFIXADMIN="$(grep 'MP_PASSWORD_POSTFIXADMIN=' "${apps_dir}/mariadb/.env")"
     ROUNDCUBE="$(grep 'MP_PASSWORD_ROUNDCUBE=' "${apps_dir}/mariadb/.env")"
-    #PA_SMTP_SERVER="$(get_MP_D_CONTAINER_x "${config_file}" "mail" "mail")"
     PA_SMTP_CLIENT="$(get_MP_D_CONTAINER_x "${config_file}" "web" "pa")"
     FQDN_MAIL="$(get_MP_FQDN_x "${config_file}" "mail")"
     FQDN_SMTP="$(get_MP_FQDN_x "${config_file}" "smtp")"
